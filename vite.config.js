26f1d208f2e2f3d5cca3bc1c02ede3475ae00476
Patryk Cieślak - Bendar Multimedia <patryk@bendar.eu>
2023-12-23 23:12:56 +0100

    v 1.0.0

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,50 @@
 import { defineConfig } from 'vite'
+import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
+import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
 import vue from '@vitejs/plugin-vue'
+const pkg = require('./package.json');
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  build: {
+    outDir: `dist/${pkg.version}`,
+    cssCodeSplit: true,
+    sourcemap: false,
+    manifest: true,
+    rollupOptions: {
+      output: {
+        entryFileNames: `js/bendar-bmi-calculator.${pkg.version}.js`,
+        chunkFileNames: `js/bendar-bmi-calculator-vendors.${pkg.version}.js`,
+        assetFileNames: `css/bendar-bmi-calculator.${pkg.version}.[ext]`,
+      },
+    },
+  },
+
+  server: {
+    port: 3000,
+  },
+
+  define: {
+    // Add any global constants here if needed
+  },
+
+  resolve: {
+    alias: {
+      // Add any aliases here if needed
+    },
+  },
+
+  optimizeDeps: {
+    include: ['vue'],
+  },
+  plugins: [
+    vue(),
+    AutoImport({
+      resolvers: [ElementPlusResolver()],
+    }),
+    Components({
+      resolvers: [ElementPlusResolver()],
+    }),
+  ],
 })
